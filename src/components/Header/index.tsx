147c7ac2a01f5ff8a46be80e51efb8e76f4040e0
lemoscaio <caio_lemos94@hotmail.com>
2022-11-21 06:11:32 -0300

    feat: add wrapper to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 
 import MortyIcon from "/morty.png"
 
-import { Navbar, Nav } from "react-bootstrap"
+import { Container, Navbar, Nav } from "react-bootstrap"
 
 export default function Header() {
 	const navigate = useNavigate()
@@ -19,50 +19,52 @@ export default function Header() {
 	return (
 		<header>
 			<Navbar
-				className="px-3 py-2 border-bottom"
+				className="px-3 py-2 border-bottom shadow-sm"
 				expand="md"
 				sticky="top"
 				collapseOnSelect
 			>
-				<Navbar.Brand className="me-2">
-					<img
-						src={MortyIcon}
-						height="50"
-						className="me-3"
-					/>
-					Rick and Morty
-				</Navbar.Brand>
-				<Navbar.Toggle aria-controls="basic-navbar-nav" />
-				<Navbar.Collapse
-					id="responsive-navbar-nav"
-					className="justify-content-end"
-				>
-					<Nav
-						justify
-						activeKey={selectedOption}
-						onSelect={(menuOption) => onMenuClick(menuOption)}
-						className="align-items-center"
+				<Container>
+					<Navbar.Brand className="me-2">
+						<img
+							src={MortyIcon}
+							height="50"
+							className="me-3"
+						/>
+						Rick and Morty
+					</Navbar.Brand>
+					<Navbar.Toggle aria-controls="basic-navbar-nav" />
+					<Navbar.Collapse
+						id="responsive-navbar-nav"
+						className="justify-content-end"
 					>
-						<Nav.Item
-							className="py-sm-2"
-							bsPrefix="nav-item"
+						<Nav
+							justify
+							activeKey={selectedOption}
+							onSelect={(menuOption) => onMenuClick(menuOption)}
+							className="align-items-center"
 						>
-							<Nav.Link eventKey="characters">Characters</Nav.Link>
-						</Nav.Item>
-						<Nav.Item
-							className="py-sm-2"
-							bsPrefix="nav-item"
-						>
-							<Nav.Link eventKey="locations">Locations</Nav.Link>
-						</Nav.Item>
-						<Nav.Item
-							className="py-sm-2"
-							bsPrefix="nav-item"
-						>
-							<Nav.Link eventKey="episodes">Episodes</Nav.Link>
-						</Nav.Item>
-					</Nav>
-				</Navbar.Collapse>
+							<Nav.Item
+								className="py-sm-2"
+								bsPrefix="nav-item"
+							>
+								<Nav.Link eventKey="characters">Characters</Nav.Link>
+							</Nav.Item>
+							<Nav.Item
+								className="py-sm-2"
+								bsPrefix="nav-item"
+							>
+								<Nav.Link eventKey="locations">Locations</Nav.Link>
+							</Nav.Item>
+							<Nav.Item
+								className="py-sm-2"
+								bsPrefix="nav-item"
+							>
+								<Nav.Link eventKey="episodes">Episodes</Nav.Link>
+							</Nav.Item>
+						</Nav>
+					</Navbar.Collapse>
+				</Container>
 			</Navbar>
 		</header>
 	)
